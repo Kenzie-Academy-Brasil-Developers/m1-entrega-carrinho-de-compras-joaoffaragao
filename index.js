af09646538d950699e5b486c9bebad06468d47a9
Joao Aragao <joao_felipe_fa@hotmail.com>
2022-05-10 15:27:18 -0400

    cabecalho finalizado

--- index.js
+++ index.js
@@ -1,7 +1,81 @@
 const productsCart = [
     {
         id: 1,
-        name: "",
-        price: 0.00
-    }
+        name: "banana",
+        price: 10.00
+    },
+    {
+        id: 2,
+        name: "manga",
+        price: 10.00
+    },
+    {
+        id: 3,
+        name: "amora",
+        price: 10.00
+    },
+    {
+        id: 4,
+        name: "uva",
+        price: 10.00
+    },
+
 ]
+
+
+
+
+function criaCabecalho(cabecalhoList){
+
+    let cabecalho = document.createElement("ul")
+    cabecalho.classList.add("cabecalho")
+
+    cabecalhoList.forEach(valor => {
+        let item = document.createElement("li")
+        let paragrafo = document.createElement("p")
+        paragrafo.innerText = valor
+
+        paragrafo.classList.add("itemCabecalho") 
+        
+        item.appendChild(paragrafo)
+
+        cabecalho.appendChild(item)
+
+    })
+
+    return cabecalho
+}
+
+
+function criaCardProduto(produto){
+
+    
+
+}
+
+
+const body = document.body
+
+const main = document.createElement("main")
+
+const finalizarButton = document.createElement("button")
+finalizarButton.innerText = "Finalizar"
+
+let soma = 0 
+
+let productDetalisList = [soma,finalizarButton]
+
+
+let cabecalhoProdutosList = ["Item","Valor"]
+
+let listaProdutos = document.createElement("ul")
+
+let productDetalisElement = document.createElement("ul")
+
+let cabecalhoProdutos =  criaCabecalho(cabecalhoProdutosList)
+
+main.appendChild(cabecalhoProdutos)
+
+
+body.appendChild(main)
+
